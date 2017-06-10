b0a3c4c99ba2b857e5a21fecf8fd652394f9b539
vrajau <vincent.rajau@utt.fr>
2017-06-10 07:01:19 +0200

    FEATURE: initialize folders

--- functions/tacherontabFunctions.sh
+++ functions/tacherontabFunctions.sh
@@ -8,4 +8,8 @@ init()
   if [ ! -d $CONFIGUSER ];then
     $(mkdir $CONFIGUSER)
   fi
+
+  if [ ! -f $CONFIGALL ];then
+    $(touch $CONFIGALL)
+  fi
 }
--- tacherontab.sh
+++ tacherontab.sh
@@ -3,7 +3,7 @@
 source functions/tacherontabFunctions.sh
 
 if [ $EUID -eq 0 ];then
-  init
+  init #initialize folder
 else
   echo "You must be root user to use tacherontab command">&2
 fi
