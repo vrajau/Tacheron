9c8b1d18beabbe8e885b3de42a3f025dcca53069
vrajau <vincent.rajau@utt.fr>
2017-06-10 07:52:02 +0200

    FEATURE: Function to select right config file

--- functions/tacherontabFunctions.sh
+++ functions/tacherontabFunctions.sh
@@ -20,3 +20,13 @@ checkUserId()
   test=$(id -u $1>/dev/null 2>&1)
   echo $? #Return 0 if user exist, else 1
 }
+
+#Select the proper config to check if it's general config or user config
+selectConfig()
+{
+  configFile=$CONFIGALL
+  if [ ! -z $1 ];then
+    configFile=$CONFIGUSER"tacherontab"$1
+  fi
+  echo $configFile
+}
--- tacherontab.sh
+++ tacherontab.sh
@@ -5,7 +5,7 @@ source functions/tacherontabFunctions.sh
 if [ $EUID -eq 0 ];then
   init #initialize folder
   user=""
-  while getopts "u:" option; do
+  while getopts "u:rel" option; do
     case $option in
       u)
         if [ $(checkUserId $OPTARG ) -eq 0 ];then
@@ -14,8 +14,15 @@ if [ $EUID -eq 0 ];then
         else
           echo "The specified user does not exist. Tacherontab is going to use general configuration">&2
         fi
+        ;;
+      l)
+        selectConfig $user
+      ;;
       esac
     done
+
+
+
 else
   echo "You must be root user to use tacherontab command">&2
 fi
