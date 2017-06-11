1bae373634b93c0c9867a8aed4b87e37e70af62c
vrajau <vincent.rajau@utt.fr>
2017-06-12 01:19:55 +0200

    FEATURE: Automated

--- tacheron.sh
+++ tacheron.sh
@@ -17,13 +17,17 @@ fi
 
 
 if [ "$EUID" -eq 0 ] || [ $(isUserAllowed $currentUser) = true ];then
-  while read task;do
-    if [ ! -z $(validField "$task") ];then
-      timeField=$(echo $task|awk '{for(i=1;i<=6;i++) print $i}')
-      commande=$(echo $task|awk '{for(i=7;i<=NF;i++) print $i}')
-       analyseAndExecute $timeField "$commande"
-    fi
-  done < $CONFIGALL
+  while true;
+  do
+      while read task;do
+        if [ ! -z $(validField "$task") ];then
+          timeField=$(echo $task|awk '{for(i=1;i<=6;i++) print $i}')
+          commande=$(echo $task|awk '{for(i=7;i<=NF;i++) print $i}')
+           analyseAndExecute $timeField "$commande"
+        fi
+      done < $CONFIGALL
+      $(sleep 0.7)
+  done
 else
   echo "You are not allowed to use Tacheron. Contact your administrator">&2
   exit 1
